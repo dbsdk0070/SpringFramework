cfba1fbf73783206446e8c1a213a42e9e4667841
dbsdk0070 <76901364+dbsdk0070@users.noreply.github.com>
2021-06-18 17:23:41 +0900

    Add files via upload

---
+++ 09_AOP_LogAdvice/src/main/java/com/spring/biz/common/Log4jAdvice.java
@@ -0,0 +1,8 @@
+package com.spring.biz.common;
+
+public class Log4jAdvice {
+
+	public void printLogging() {
+		System.out.println("[공통로그] 비즈니스 로직 수행 전  로그");	//Log4jAdvice에 있는 printLogging()메서드 생성, 실행
+	}
+}
